1df5a3d65943c4038370a9621c3d51ce879842e7
unknown <vgovilkar@VGOVIKAR7-T430.oww.root.lcl>
2014-03-11 13:47:53 -0500

    Nth Prime

--- 10001stprime.py
+++ 10001stprime.py
@@ -1,20 +1,20 @@
 #!/usr/local/bin/python2.7
-    
-import math;
-pnos=[2];
-piter=1;
-i=3;
-while(len(pnos)<10001):
-  prime=True;
-  
-  for x in pnos:
-    if(x>math.sqrt(i)):
-      break;
-    if(i%x==0):
-      prime=False;
-  if(prime):
-    pnos.append(i);
-  i+=2;
-
-print "hello";
-print pnos[-1];
+import sys
+import math
+def NthPrime(num):
+	pnos=[2]
+	piter=1
+	i=3
+	while(len(pnos)<num):
+		prime=True
+		for x in pnos:
+			if(x>math.sqrt(i)):
+				break;
+			if(i%x==0):
+				prime=False;
+		if(prime):
+			pnos.append(i)
+		i+=2
+	return pnos[-1]
+if(__name__=='__main__'):
+	print NthPrime(int((sys.argv)[1]))
